607d33809d554d66d238b3421a54e3426aab9f0d
Jeff Hilnbrand <jahilnbrand@gmail.com>
2016-01-11 03:03:25 -0500

    init pages

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 $(function() {
+
     $(document).mousemove(function(event) {
-        var width = event.pageX / document.body.clientWidth;
-        var hue = (125 + width*120);
 
-        document.body.style.backgroundColor = "hsl(" + hue.toString() + ",93%,74%)";
+        var width = ( event.pageX / document.body.clientWidth ) * 360;
+        var hue = (180 + width);
+        var imageno = 10 * ((18 + Math.floor(width/10)) % 36);
+
+        document.body.style.backgroundColor = "hsl(" + hue.toString() + ",85%,70%)"; // change background color based on mouse position
+
+        document.getElementById('favicon-image').href = 'img/favicon/favicon-' + imageno.toString() + '.png'; // change favicon to match color of background
+
     });
-});
 
+    $(window).bind("beforeunload", function() {
+
+        document.getElementById('favicon-image').href = 'img/favicon/favicon-0.png'; // change favicon to red upon window close
+
+    });
+
+});
